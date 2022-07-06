248decdb55a100f8f8433ff650ae37ea9aeac79e
Suheyl Esadli <79663047+suheyl-code@users.noreply.github.com>
2022-07-06 15:11:52 +0300

    ..

--- WindowsFormsApp-EntityFramework/Form1.Designer.cs
+++ WindowsFormsApp-EntityFramework/Form1.Designer.cs
@@ -214,7 +214,7 @@
             this.Controls.Add(this.dataGridView1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "With Entity Framework";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
